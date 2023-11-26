8bf49a4df2582ce90ee62db911dacc05d3691611
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2023-11-26 15:16:13 +0700

    Add clear history

--- app/history.tsx
+++ app/history.tsx
@@ -9,25 +9,42 @@ import {
 import { Text, View } from "../components/Themed";
 import { useContext } from "react";
 import { HistoryContext } from "../context/history";
-import { AntDesign } from "@expo/vector-icons";
 import { HistoryRow } from "../components/History";
 import HistoryHead from "../components/History/HistoryHead";
 
 export default function ModalScreen() {
-  const { calculationHistories } = useContext(HistoryContext);
+  const { calculationHistories, clearHistory } = useContext(HistoryContext);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>History</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      <HistoryHead />
-      {calculationHistories.map((history, index) => (
-        <HistoryRow isOdd={!!(index % 2)} key={index} {...history} />
-      ))}
+
+      {calculationHistories.length === 0 ? (
+        <Text
+          style={{
+            fontSize: 14,
+            color: "#fff",
+            textAlign: "center",
+            paddingTop: 20,
+          }}
+        >
+          You don't have any record
+        </Text>
+      ) : (
+        <>
+          <TouchableOpacity
+            style={{ marginLeft: "auto", paddingBottom: 12 }}
+            onPress={clearHistory}
+          >
+            <Text style={{ fontSize: 14, fontWeight: "600" }}>Clear</Text>
+          </TouchableOpacity>
+          <HistoryHead />
+          {calculationHistories.map((history, index) => (
+            <HistoryRow isOdd={!!(index % 2)} key={index} {...history} />
+          ))}
+        </>
+      )}
+
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </ScrollView>
   );
@@ -37,8 +54,8 @@ const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     alignItems: "center",
-    justifyContent: "center",
     paddingVertical: 20,
+    paddingHorizontal: 20,
   },
   title: {
     fontSize: 20,
